843fef6f9fdf393a66c0c9ab7dd518477164201a
Christopher Miller <git@chrismiller.xyz>
2025-03-23 13:49:43 -0700

    Working http endpoint

--- motor-controller/build.rs
+++ motor-controller/build.rs
@@ -1,3 +1,16 @@
+use std::fs::read_to_string;
+
+#[derive(Debug, serde::Deserialize)]
+pub struct Config {
+    wifi_ssid: String,
+    wifi_psk: String,
+}
+
 fn main() {
+    let config: Config = toml::from_str(&read_to_string("cfg.toml").unwrap()).unwrap();
+
+    println!("cargo:rustc-env=CONFIG_WIFI_SSID={}", config.wifi_ssid);
+    println!("cargo:rustc-env=CONFIG_WIFI_PSK={}", config.wifi_psk);
+
     embuild::espidf::sysenv::output();
 }
